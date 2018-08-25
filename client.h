28d3f44b63c79c06496434bb7d11cbbdb34ae235
Sigboom <292382967@qq.com>
2018-08-26 00:46:44 +0800

    nothing

---
+++ client.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: client.h
+	> Author: Daniel
+	> Mail: 292382967@qq.com
+	> Created Time: 三  7/18 18:12:11 2018
+ ************************************************************************/
+
+#ifndef _CLIENT_H
+#define _CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mynet.h"
+
+#endif
