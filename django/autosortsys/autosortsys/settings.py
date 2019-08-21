f914d2f6411ae78f173d0a9a85c8650b59c12fc8
Sigboom <sigboom@163.com>
2019-08-21 20:09:36 +0800

    update mysql

--- django/autosortsys/autosortsys/settings.py
+++ django/autosortsys/autosortsys/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'autosortsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'autosortsysdb',
+        'USER': 'root',
+        'PASSWORD': '6940588666035',
+        'HOST': '127.0.0.1',
+        'POST': 3306,
     }
 }
 
